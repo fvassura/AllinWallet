01afe46369b4e5d4d1b759d7394a27ea93bba046
TS\fabiov <fabio.vassura@t-soft.it>
2024-10-14 08:44:25 +0200

    primo commit
    (cherry picked from commit 2851779e6fd9687e5d3fb804e6d432328f33de6d)

---
+++ MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿namespace AllinWallet
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times ciao";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+    }
+
+}
